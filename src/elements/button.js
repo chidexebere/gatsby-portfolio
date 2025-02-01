7ea6e85fbf5db93de111e2f430171cc14059b565
Chidiebere Onyegbuchulem <chidex5@gmail.com>
2025-02-01 09:38:46 +0100

    feat: replace Gatsby with Nextjs

--- src/elements/button.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-const Button = props => {
-  const { variant, children } = props
-  const computedClass = variant ? `button ${variant}` : `button`
-  return <button className={computedClass}>{children}</button>
-}
-
-export default Button
