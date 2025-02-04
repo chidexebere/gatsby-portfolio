f95d2a61eb01f6e867797508705c5530ab5c7392
Chidiebere Onyegbuchulem <chidex5@gmail.com>
2025-02-04 15:46:39 +0100

    feat: modify intro

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,8 +5,10 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Chidi.</h1>
-        <p className='mt-3 font-light text-muted-foreground'>
+        <h1 className='title text-green-600 no-underline'>
+          Hey, I&#39;m Chidi.
+        </h1>
+        <p className='mt-3 text-xl font-light text-muted-foreground'>
           I&#39;m a software engineer based in Lagos, Nigeria. I&#39;m
           passionate about creating products with seamless experiences and
           exceptional design.
