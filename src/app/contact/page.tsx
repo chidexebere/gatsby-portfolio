1007a97033427bb789fbd8555dd39b8d57c66782
Chidiebere Onyegbuchulem <chidex5@gmail.com>
2025-02-05 12:21:17 +0100

    feat: modify contact component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,10 +2,14 @@ import ContactForm from '@/components/contact-form'
 
 export default function Contact() {
   return (
-    <section className='pb-24 pt-40'>
+    <section className=''>
       <div className='container max-w-3xl'>
-        <h2 className='title'>Let&apos;s talk about your project</h2>
-
+        <h2 className='title text-green-600'>
+          Let&apos;s talk about your project
+        </h2>
+        <span className='mt-2 text-xs text-muted-foreground'>
+          We help companies and individuals build out their digital presence.
+        </span>
         <ContactForm />
       </div>
     </section>
